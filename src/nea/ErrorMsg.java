372c3210f3b47436aec0463c0428f8b225519f0e
Michal <michalpl2003@gmail.com>
2021-08-06 21:52:04 +0100

    Improvements to the ErrorMsg class

--- src/nea/ErrorMsg.java
+++ src/nea/ErrorMsg.java
@@ -20,9 +20,15 @@ public class ErrorMsg {
     static int INPUT_LENGTH_ERROR = 3;
     static int INPUT_DETAILS_MISMATCH_ERROR = 4;
     static int INVALID_LOGIN_DETAILS_ERROR = 5;
+    /**
+     * This must have an accompanying message
+     */
     static int NOTHING_SELECTED_ERROR = 6;
     static int CANNOT_REMOVE_ERROR = 7;
     static int CANNOT_EDIT_ERROR = 8;
+    /**
+     * This must have an accompanying message
+     */
     static int INVALID_INPUT_ERROR = 9;
 
     public static void throwError() {
@@ -38,60 +44,83 @@ public class ErrorMsg {
     }
 
     public static void throwError(int error_id, String customMessage) {
+        String msg = "";
         switch (error_id) {
             case 0:
-                JOptionPane.showMessageDialog(null, customMessage.isEmpty() ? "One or more of the input fields is empty" : customMessage, "Empty Input Field Error", JOptionPane.ERROR_MESSAGE);
+                msg = customMessage.isEmpty() ? "One or more of the input fields is empty" : customMessage;
+
+                JOptionPane.showMessageDialog(null, msg, "Empty Input Field Error", JOptionPane.ERROR_MESSAGE);
                 System.out.println("-------------------------------");
-                System.out.println("One or more of the input fields is empty");
+                System.out.println(msg);
                 break;
             case 1:
-                JOptionPane.showMessageDialog(null, customMessage.isEmpty() ? "The 'Add category' option is not a valid category" : customMessage, "Invalid Category Selected Error", JOptionPane.ERROR_MESSAGE);
+                msg = customMessage.isEmpty() ? "The 'Add category' option is not a valid category" : customMessage;
+
+                JOptionPane.showMessageDialog(null, msg, "Invalid Category Selected Error", JOptionPane.ERROR_MESSAGE);
                 System.out.println("-------------------------------");
-                System.out.println("The 'Add category' option is not a valid category");
+                System.out.println(msg);
                 break;
             case 2:
-                JOptionPane.showMessageDialog(null, customMessage + " under this name already exists", "Already Exists Error", JOptionPane.ERROR_MESSAGE);
+                msg = customMessage.isEmpty() ? "Category name under this name already exists" : customMessage;
+
+                JOptionPane.showMessageDialog(null, msg, "Already Exists Error", JOptionPane.ERROR_MESSAGE);
                 System.out.println("-------------------------------");
-                System.out.println(customMessage + " under this name already exists");
+                System.out.println(msg);
                 break;
             case 3:
-                JOptionPane.showMessageDialog(null, "The entered " + customMessage + " is too long", "Input Length Error", JOptionPane.ERROR_MESSAGE);
+                msg = "The entered " + customMessage + " is too long";
+
+                JOptionPane.showMessageDialog(null, msg, "Input Length Error", JOptionPane.ERROR_MESSAGE);
                 System.out.println("-------------------------------");
-                System.out.println("Category name is too long");
+                System.out.println(msg);
                 break;
             case 4:
-                JOptionPane.showMessageDialog(null, customMessage.isEmpty() ? "Login details do not match, check if you have entered them correctly" : customMessage, "Input Details Mismatch Error", JOptionPane.ERROR_MESSAGE);
+                msg = customMessage.isEmpty() ? "Login details do not match, check if you have entered them correctly" : customMessage;
+
+                JOptionPane.showMessageDialog(null, msg, "Input Details Mismatch Error", JOptionPane.ERROR_MESSAGE);
                 System.out.println("-------------------------------");
-                System.out.println("Login details do not match, check you have entered them correctly");
+                System.out.println(msg);
                 break;
             case 5:
-                JOptionPane.showMessageDialog(null, customMessage.isEmpty() ? "Incorrect login details, check you have entered them correctly" : customMessage, "Invalid Login Details Error", JOptionPane.ERROR_MESSAGE);
+                msg = customMessage.isEmpty() ? "Incorrect login details, check you have entered them correctly" : customMessage;
+
+                JOptionPane.showMessageDialog(null, msg, "Invalid Login Details Error", JOptionPane.ERROR_MESSAGE);
                 System.out.println("-------------------------------");
-                System.out.println("Incorrect login details");
+                System.out.println(msg);
                 break;
             case 6:
-                JOptionPane.showMessageDialog(null, customMessage.isEmpty() ? "No row selected" : customMessage, "Nothing Selected Error", JOptionPane.ERROR_MESSAGE);
+                msg = customMessage.isEmpty() ? "No row selected" : customMessage;
+
+                JOptionPane.showMessageDialog(null, msg, "Nothing Selected Error", JOptionPane.ERROR_MESSAGE);
                 System.out.println("-------------------------------");
-                System.out.println("No row selected");
+                System.out.println(msg);
                 break;
             case 7:
-                JOptionPane.showMessageDialog(null, "This is the default row and cannot be removed", "Cannot Remove Error", JOptionPane.ERROR_MESSAGE);
+                msg = customMessage.isEmpty() ? "This is the default row and cannot be removed" : customMessage;
+
+                JOptionPane.showMessageDialog(null, msg, "Cannot Remove Error", JOptionPane.ERROR_MESSAGE);
                 System.out.println("-------------------------------");
-                System.out.println("Default row cannot be removed");
+                System.out.println(msg);
                 break;
             case 8:
-                JOptionPane.showMessageDialog(null, "This is the default row and cannot be edited", "Cannot Edit Error", JOptionPane.ERROR_MESSAGE);
+                msg = customMessage.isEmpty() ? "This is the default row and cannot be edited" : customMessage;
+
+                JOptionPane.showMessageDialog(null, msg, "Cannot Edit Error", JOptionPane.ERROR_MESSAGE);
                 System.out.println("-------------------------------");
-                System.out.println("Default row cannot be edited");
+                System.out.println(msg);
                 break;
             case 9:
-                JOptionPane.showMessageDialog(null, "You must enter something for the " + customMessage, "Invalid Input Error", JOptionPane.ERROR_MESSAGE);
+                msg = "You must enter something for the " + customMessage;
+
+                JOptionPane.showMessageDialog(null, msg, "Invalid Input Error", JOptionPane.ERROR_MESSAGE);
                 System.out.println("-------------------------------");
-                System.out.println("Empty " + customMessage);
+                System.out.println(msg);
             default:
-                JOptionPane.showMessageDialog(null, customMessage.isEmpty() ? "An error has occurred" : customMessage, "Error", JOptionPane.ERROR_MESSAGE);
+                msg = customMessage.isEmpty() ? "An error has occurred" : customMessage;
+
+                JOptionPane.showMessageDialog(null, msg, "Error", JOptionPane.ERROR_MESSAGE);
                 System.out.println("-------------------------------");
-                System.out.println("An error has occurred");
+                System.out.println(msg);
                 break;
         }
 
